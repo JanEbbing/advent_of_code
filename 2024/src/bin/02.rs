9eca60ee1375d4c722c3252a62023c893ac7ee8e
Jan Ebbing <Jan.Ebbing@gmx.de>
2024-12-02 20:20:07 +0000

    Day 2 Part 2

--- 2024/src/bin/02.rs
+++ 2024/src/bin/02.rs
@@ -23,6 +23,23 @@ fn main() -> Result<()> {
     //region Part 1
     println!("=== Part 1 ===");
 
+    fn first_unsafe_index(report: &Vec<i32>) -> i32 {
+        if report.len() < 2 {
+            return -1;
+        }
+        let decreasing: bool = report[0] > report[1];
+        for i in 1..report.len() {
+            let mut cur_difference = report[i] - report[i-1];
+            if decreasing {
+                cur_difference = -cur_difference;
+            }
+            if cur_difference < 1 || cur_difference > 3 {
+                return (i-1).try_into().unwrap();
+            }
+        }
+        return -1;
+    }
+
     fn part1<R: BufRead>(reader: R) -> Result<i32> {
         let mut reports: Vec<Vec<i32>> = Vec::new();
         for line in reader.lines().map(|l| l.unwrap()) {
@@ -30,23 +47,7 @@ fn main() -> Result<()> {
         }
         let mut result = 0;
         for report in reports {
-            if report.len() < 2 {
-                result += 1;
-                continue;
-            }
-            let decreasing: bool = report[0] > report[1];
-            let mut report_is_safe: bool = true;
-            for i in 1..report.len() {
-                let mut cur_difference = report[i] - report[i-1];
-                if decreasing {
-                    cur_difference = -cur_difference;
-                }
-                if cur_difference < 1 || cur_difference > 3 {
-                    report_is_safe = false;
-                    break;
-                }
-            }
-            if report_is_safe {
+            if first_unsafe_index(&report) < 0 {
                 result += 1;
             }
         }
@@ -61,17 +62,41 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<i32> {
+        let mut reports: Vec<Vec<i32>> = Vec::new();
+        for line in reader.lines().map(|l| l.unwrap()) {
+            reports.push(line.split(" ").map(|n| n.parse::<i32>().unwrap()).collect::<Vec<i32>>());
+        }
+        let mut result = 0;
+        for report in reports {
+            let unsafe_index_candidate_1 = first_unsafe_index(&report);
+            if unsafe_index_candidate_1 < 0 {
+                result += 1;
+                continue;
+            }
+            let unsafe_index_candidate_2 = unsafe_index_candidate_1 + 1;
+            let mut report_without_candidate_1 = report.clone();
+            report_without_candidate_1.remove(unsafe_index_candidate_1.try_into().unwrap());
+            let mut report_without_candidate_2 = report.clone();
+            report_without_candidate_2.remove(unsafe_index_candidate_2.try_into().unwrap());
+            let mut report_without_candidate_3 = report.clone();
+            report_without_candidate_3.remove(0);
+            let mut report_without_candidate_4 = report.clone();
+            report_without_candidate_4.remove(1);
+            if  [report_without_candidate_1, report_without_candidate_2, report_without_candidate_3, report_without_candidate_4].into_iter().map(|r| first_unsafe_index(&r) < 0).filter(|b| *b).count() > 0 {
+                result += 1;
+            }
+        }
+        return Ok(result);
+    }
+
+    assert_eq!(4, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
